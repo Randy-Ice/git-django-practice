6d367169da92e8114ea2c58bd46d698fd4a01774
Randy Ainsworth <randyainsworth.789@gmail.com>
2025-06-06 11:57:15 -0700

    add model data

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+import uuid
 
+from django.db import models
+from django.conf import settings
 # Create your models here.
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='blog/images')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title} by {self.author}'
